09452949f74d5ff0d1e5ff890661a1787759f830
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-08 10:51:04 -0300

    Adicioanando conteúdo de comandos de interação com páginas JavaScript do Curso em Vídeo

---
+++ Curso em Vídeo/JavaScript/Aula 1 - O que o JavaScript é capaz de fazer/comandos_console.js
@@ -0,0 +1,19 @@
+// Testes na página do google
+
+// Alerta na página do google
+alert("Olá, mundo!");
+
+// Mudar cor de fundo da tela
+document.body.style.background = "black";
+
+// Selecionar uma imagem e deixa-la com um filtro preto e branco
+document.querySelector("img#hplogo").style.filter = "grayscale(100%)";
+
+// Esconder uma imagem
+document.querySelector("img#hplogo").style.visibility = "hidden";
+
+// Testes na página do G1
+
+// Modificar título de notícia
+document.querySelector("span.btsn-hl-title").innerText =
+  "Curso em Vídeo de JavaScript é o melhor do YouTube!";
