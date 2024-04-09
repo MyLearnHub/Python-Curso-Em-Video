6870058022f58a92fbf155df29bd96290b1da36e
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-09 11:45:33 -0300

    Adicionando conteúdo de operadores em JavaScript do Curso em Vídeo

---
+++ Curso em Vídeo/JavaScript/Módulo B - Comandos Básicos do JavaScript/Aula 7 - Operadores (Parte 1)/testes_terminal.js
@@ -0,0 +1,35 @@
+console.log(5 + 2);
+console.log(9 % 2);
+console.log(9 / 2);
+console.log(5 + 3 / 2);
+
+// console.log(5 + / 2); - Erro
+
+console.log((5 + 3) / 2);
+
+var f = 3 % 2 + 4 / 2;
+console.log(f);
+
+var num = 8
+
+console.log(num);
+console.log(num +=2);
+console.log(num);
+console.log(num %= 2);
+num = 3
+console.log(num **= 2);
+
+var n = 10
+console.log(n);
+
+console.log(n++);
+console.log(n);
+
+console.log(n--);
+console.log(n);
+
+console.log(++n);
+console.log(n);
+
+console.log(--n);
+console.log(n);
