b9873cc76032e163c057a16436fd329d7f3ed71c
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-09 09:03:07 -0300

    Adicionando conteúdo de testes de variáveis em JavaScript do Curso em Vídeo

---
+++ Curso em Vídeo/JavaScript/Módulo B - Comandos Básicos do JavaScript/Aula 5 - Variáveis e Tipos Primitivos/testes_terminal.js
@@ -0,0 +1,57 @@
+console.log("Oi");
+
+console.log(3 + 2);
+
+var nome = "Gustavo";
+console.log(nome);
+
+nome = "Paulo";
+console.log(nome);
+
+//.exit - fecha o console
+
+// node - abre o terminal no vscode
+
+var idade = 18;
+var salario = 1550.35;
+var sexo = "M";
+
+console.log(idade);
+console.log(salario);
+console.log(sexo);
+
+var n1 = 8;
+var n2 = 5;
+
+console.log(n1);
+console.log(n2);
+console.log(n1 + n2);
+
+// Ctrl + L - Limpa a tela do terminal
+
+var a = 10;
+var A = 20;
+
+console.log(a);
+console.log(A);
+
+var b = 30;
+console.log(B);
+
+var n = 200;
+console.log(n);
+console.log(typeof n);
+
+n = "Google";
+console.log(typeof n);
+
+console.log(typeof 6);
+console.log(typeof 6.5);
+console.log(typeof "6.5");
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function () {});
+console.log(typeof undefined);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof null);
