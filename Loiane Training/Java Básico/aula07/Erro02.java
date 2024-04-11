d80ac3af34122de338d230df0c1eab292a34c203
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-11 16:11:05 -0300

    Adicionando conteúdos de Java da Loiane Training

---
+++ Loiane Training/Java Básico/aula07/Erro02.java
@@ -0,0 +1,6 @@
+public class Erro02 {
+    public static void main(String[] args) {
+        int cont = 1;
+        System.out.println("Voce digitou " + cont);
+    }
+}
---
+++ Loiane Training/Java Básico/aula07/Erro03.java
@@ -0,0 +1,6 @@
+public class Erro03 {
+    public static void main(String[] args) {
+        // System.out.println("Voce digitou: ");
+        System.out.println("Voce digitou: " + 1 / 0);
+    }
+}
