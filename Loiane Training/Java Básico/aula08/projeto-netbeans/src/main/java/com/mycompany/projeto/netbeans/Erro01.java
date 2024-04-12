d4ee8593d7f5690b0edc75fc204f7e4892b50fe4
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-12 02:58:17 -0300

    Adicionando estudos sobre criação de projetos Java nas IDEs Netbeans e Eclipse do curso de Java da Loiane

---
+++ Loiane Training/Java Básico/aula08/projeto-netbeans/src/main/java/com/mycompany/projeto/netbeans/Erro01.java
@@ -0,0 +1,7 @@
+package com.mycompany.projeto.netbeans;
+
+public class Erro01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World Erro 01");
+    }
+}
---
+++ Loiane Training/Java Básico/aula08/projeto-netbeans/src/main/java/com/mycompany/projeto/netbeans/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.mycompany.projeto.netbeans;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
