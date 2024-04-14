ef373c9fc89c8defb551b4c9f261ccfb31363d28
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-14 06:03:51 -0300

    Iniciando exercícios de estruturas de repetição em Java da Loiane

---
+++ Loiane Training/Java Básico/aula17/labs/Exer01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exer01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean notaValida = false;
+
+        do {
+            System.out.println("Entre com uma nota (0 - 10):");
+            int nota = scan.nextInt();
+
+            if (nota >= 0 && nota <= 10) {
+                System.out.println("Você digitou: " + nota);
+                notaValida = true;
+            } else {
+                System.out.println("Nota inválida, digite novamente.");
+            }
+        } while (!notaValida);
+    }
+}
