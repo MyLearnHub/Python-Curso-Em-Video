6cb09de20541909a86024734f3918ca4498965c0
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-14 06:49:52 -0300

    Finalizando correção dos exercícios de estrutura de repetição em Java da Loiane

--- Loiane Training/Java Básico/aula17/labs/Exer30.java
+++ Loiane Training/Java Básico/aula17/labs/Exer30.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Exer30 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Entre com o número para gerar a tabuada:");
         int num = scan.nextInt();
-        
+
         boolean invalido = true;
         int numFinal, numInicio;
-        
+
         do {
-            System.out.println("Entre com o início da tabuada");
+            System.out.println("Entre com o início da tabuada:");
             numInicio = scan.nextInt();
 
-            System.out.println("Entre como final da tabuada");
+            System.out.println("Entre como final da tabuada:");
             numFinal = scan.nextInt();
 
-            if (numFinal > numInicio){
+            if (numFinal > numInicio) {
                 invalido = false;
             }
         } while (invalido);
@@ -26,9 +26,9 @@ public class Exer30 {
         System.out.println("Começar por: " + numInicio);
         System.out.println("Terminar em: " + numFinal);
         System.out.println();
-        
-        for (int i=numInicio; i<=numFinal; i++){
-            System.out.println(num + " x " + i + " = " + (num*i));
+
+        for (int i = numInicio; i <= numFinal; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
         }
     }
 }
--- Loiane Training/Java Básico/aula17/labs/Exer33.java
+++ Loiane Training/Java Básico/aula17/labs/Exer33.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Exer33 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Digite o valor de n");
+
+        System.out.println("Digite o valor de n:");
         int n = scan.nextInt();
         double soma = 0;
-        
-        for (int i=1, j=1; i<=n; i++, j+=2){
+
+        for (int i = 1, j = 1; i <= n; i++, j += 2) {
             System.out.print(i + "/" + j + " + ");
-            
-            soma += i/j;
+
+            soma += i / j;
         }
-        
+
         System.out.println("\nSoma = " + soma);
     }
 }
