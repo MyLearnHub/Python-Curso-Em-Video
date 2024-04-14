041627e3be34b3e333aa98cec4bae4cc4acab35b
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-14 06:46:55 -0300

    Continuando correção dos exercícios de estrutura de repetição em Java da Loiane

--- Loiane Training/Java Básico/aula17/labs/Exer28.java
+++ Loiane Training/Java Básico/aula17/labs/Exer28.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Exer28 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Entre com um número");
         int num = scan.nextInt();
-        
+
         boolean primo = true;
-        
-        for (int i=2; i<num; i++){
-            if (num % i == 0){
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 System.out.println("Não é primo - divisível por " + i);
                 primo = false;
             }
         }
-        
-        if (primo){
+
+        if (primo) {
             System.out.println("É número primo");
         }
     }
--- Loiane Training/Java Básico/aula17/labs/Exer29.java
+++ Loiane Training/Java Básico/aula17/labs/Exer29.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class Exer29 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Entre com um número");
+
+        System.out.println("Entre com um número:");
         int num = scan.nextInt();
-        
+
         boolean primo;
-        
-        for (int i=1; i<=num; i++){
+
+        for (int i = 1; i <= num; i++) {
             primo = true;
-        
-            for (int j=2; j<i; j++){
-                if (i % j == 0){
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
                     primo = false;
+                    break;
                 }
             }
 
-            if (primo){
+            if (primo) {
                 System.out.println(i);
             }
         }
