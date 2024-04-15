6d20d4ae5bcd14bab5c40aa5256eb6402e42b61d
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-14 22:28:24 -0300

    Adicionando conteúdos sobre break e continue em Java da Loiane

---
+++ Loiane Training/Java Básico/aula18/BreakEContinue.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class BreakEContinue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com um número:");
+        int num = scan.nextInt();
+
+        System.out.println("Entre com um limite:");
+        int max = scan.nextInt();
+
+        for (int i = num; i <= max; i++) {
+            if (i % 7 == 0) {
+                System.out.println("O valor de i é: " + i);
+                break;
+            }
+        }
+
+        for (int i = 0; i <= 4; i++) {
+            rotulo1:
+            {
+                rotulo2:
+                {
+                    rotulo3:
+                    {
+                        if (i == 1) {
+                            break rotulo1;
+                        }
+                        if (i == 2) {
+                            break rotulo2;
+                        }
+                        if (i == 3) {
+                            break rotulo3;
+                        }
+                        System.out.println("Rótulo 3");
+                    }
+                    System.out.println("Rótulo 2");
+                }
+                System.out.println("Rótulo 1");
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("Entre com um número:");
+        int num2 = scan.nextInt();
+
+        System.out.println("Entre com um limite:");
+        int max2 = scan.nextInt();
+
+        for (int i = num2; i <= max2; i++) {
+            if (i % 7 == 0) {
+                continue;
+            }
+            System.out.println("O valor de i é: " + i);
+        }
+    }
+}
