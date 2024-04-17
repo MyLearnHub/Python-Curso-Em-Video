453c158441b0a1aef5e827166d610fa96bdf837f
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-17 09:11:53 -0300

    Adicionando conteúdo sobre construtores em Java da Loiane

---
+++ Loiane Training/Java Básico/aula29/Carro2.java
@@ -0,0 +1,17 @@
+public class Carro2 {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    public Carro2(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consumoCombustivel = consumoCombustivel;
+    }
+
+    public Carro2() {}
+}
