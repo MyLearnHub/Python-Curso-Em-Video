ff1060381130d12b0f3f2425573fc1b823897446
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-17 11:50:44 -0300

    Adicionando conteúdo sobre modificadores de acesso em Java da Loiane

---
+++ Loiane Training/Java Básico/aula31/aula31alt/TesteCarroAlt.java
@@ -0,0 +1,10 @@
+package aula31alt;
+
+import aula31.Carro;
+
+public class TesteCarroAlt {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.marca = "Fiat";
+    }
+}
