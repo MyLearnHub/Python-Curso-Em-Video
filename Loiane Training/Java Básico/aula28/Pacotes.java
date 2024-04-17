f82c6935e57bac0b5d8e232f0fea8a42c4aee9b9
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-17 08:50:54 -0300

    Adicionando conteúdo sobre pacotes em Java da Loiane

---
+++ Loiane Training/Java Básico/aula28/Pacotes.java
@@ -0,0 +1,18 @@
+import aula15.labs.Exer10;
+import aula20.labs.Exer04;
+import aula20.labs.Exer05;
+import aula20.labs.Exer06;
+import aula27.Carro;
+
+public class Pacotes {
+	public static void main(String[] args) {
+		Carro carro;
+		Carro carro2;
+		
+		Exer06 exer06;
+		Exer05 exer05;
+		Exer04 exer04;
+		
+		Exer10 exer10;
+	}
+}
