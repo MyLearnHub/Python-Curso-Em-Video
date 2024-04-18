07a0900348ec40f8a6b6d781dad2a43b0c741a25
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-18 11:45:31 -0300

    Adicionando conteúdo sobre classe abstrata no Java da Loiane

---
+++ Loiane Training/Java Básico/aula41/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    public static void main(String[] args) {
+        Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();
+
+        aluno.setEndereco("Rua 2, num2");
+        professor.setEndereco("Rua 3, num3");
+
+        aluno.imprimirEtiquetaEndereco();
+        professor.imprimirEtiquetaEndereco();
+    }
+}
