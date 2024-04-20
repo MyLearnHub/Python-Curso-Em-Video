7c43b1e33f3c3bfefe705e01bc31dc4936191b70
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-19 21:21:26 -0300

    Finalizando exercícios de orientação a Objetos em Java da Loiane

---
+++ Loiane Training/Java Básico/aula43/labs/exer02/PessoaJuridica.java
@@ -0,0 +1,28 @@
+package exer02;
+
+public class PessoaJuridica extends Contribuinte {
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public double calcularImposto() {
+        return this.getRendaBruta() * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Pessoa Jurídica[";
+        s += super.toString();
+        s += " ;cnpj: " + cnpj;
+        s += " ;imposto a ser pago: " + calcularImposto();
+        s += "]";
+        return s;
+    }
+}
