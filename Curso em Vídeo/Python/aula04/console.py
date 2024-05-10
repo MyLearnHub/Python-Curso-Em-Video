0e0581b99dbc2f7e9f7dfbb647377e9fe47f851e
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-10 17:34:37 -0300

    refatorando aulas e realizando primeiro exercício do curso de python do Curso em Vídeo

--- Curso em Vídeo/Python/aula04/console.py
+++ Curso em Vídeo/Python/aula04/console.py
@@ -4,7 +4,7 @@ print(7 + 4)
 
 print('7' + '4')
 
-print('Olá' + 5) # Essa linha dará erro
+# print('Olá' + 5) # Essa linha dará erro
 
 print('Olá', 5)
 
@@ -13,7 +13,7 @@ idade = 25
 peso = 74.8
 print(nome, idade, peso)
 
-print(nome + idade + peso) # Essa linha dará erro
+# print(nome + idade + peso) # Essa linha dará erro
 
 nome = input('Qual é o seu nome?')
 idade = input('Qual é a sua idade?')
