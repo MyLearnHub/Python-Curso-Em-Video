1b7372fc020f47c64f498de9585bd6dd96395686
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-15 02:32:22 -0300

    Adicionando exercícios 14 e 15 de Python do Curso em Vídeo

---
+++ Curso em Vídeo/Python/exercicios/ex014.py
@@ -0,0 +1,4 @@
+c = float(input('Informe a temperatura em ºC: '))
+f = 9 * c / 5 + 32
+print('A temperatura de {}ºC corresponde a {}ºF'.format(c, f))
+# print('A temperatura de {0}ºC corresponde a {1}ºF'.format(c, f)) # Informando o index do argumento
