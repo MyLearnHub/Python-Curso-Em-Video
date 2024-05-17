9b3c9bee05025074244f165c8692e4682196f7c4
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-17 16:44:26 -0300

    Corrigindo exercícios de manipulação de caracteres em Python 26 e 27 do Curso em VÂídeo

--- Curso em Vídeo/Python/exercicios/ex027.py
+++ Curso em Vídeo/Python/exercicios/ex027.py
@@ -1,3 +1,10 @@
-nome = str(input('Digite seu nome completo: '))
-print('Seu primeiro nome: {}'.format(nome.split()[0]))
-print('Seu último nome: {}'.format(nome.split()[-1]))
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome) - 1]))
+
+"""nome = str(input('Digite seu nome completo: ')).split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[-1]))"""  # Alternativa
