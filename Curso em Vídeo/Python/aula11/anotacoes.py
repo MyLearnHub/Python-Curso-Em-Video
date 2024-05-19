80d01675e9d3796e9ac14a4101aa54f98d328989
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-19 02:27:14 -0300

    Adicionando conteúdo de adição de cores no terminal de Python do Curso em Vídeo

---
+++ Curso em Vídeo/Python/aula11/anotacoes.py
@@ -0,0 +1,8 @@
+"""
+Código ANSI - Style, Text e Background
+\033[0;33;44m
+
+Style - 0 (None), 1 (Bold), 4 (Underline) e 7 (Negative).
+Text - 30 (Branco), 31 (Vermelho), 32 (Verde), 33 (Amarelo), 34 (Azul), 35 (Magenta), 36 (Ciano) e 37 (Cinza).
+Background - 40 (Branco), 41 (Vermelho), 42 (Verde), 43 (Amarelo), 44 (Azul), 45 (Magenta), 46 (Ciano) e 47 (Cinza).
+"""
