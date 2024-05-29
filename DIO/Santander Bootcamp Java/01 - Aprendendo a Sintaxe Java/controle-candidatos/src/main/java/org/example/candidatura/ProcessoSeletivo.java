8bc2d642d1993439f095ccdbd437cd6fed3ea467
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-29 13:03:33 -0300

    Adicionando funcionalidade de impressão de candidatos no projeto de Processo Seletivo em Java da DIO

--- DIO/Santander Bootcamp Java/01 - Aprendendo a Sintaxe Java/controle-candidatos/src/main/java/org/example/candidatura/ProcessoSeletivo.java
+++ DIO/Santander Bootcamp Java/01 - Aprendendo a Sintaxe Java/controle-candidatos/src/main/java/org/example/candidatura/ProcessoSeletivo.java
@@ -7,6 +7,20 @@ public class ProcessoSeletivo {
         selecaoCandidatos();
     }
 
+    static void imprimirSelecionados() {
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indíce do elemento");
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº " + (indice + 1) + " é o " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
     static void selecaoCandidatos() {
         String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
 
