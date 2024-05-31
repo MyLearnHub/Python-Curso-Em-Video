3d7e1016ba720fd7d1510205726b24ed921e5be2
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-31 05:53:14 -0300

    Adicionando conteúdo sobre Enums em Java da DIO

--- DIO/Santander Bootcamp Java/aulas/exemplo-pacotes/src/main/java/contrutores/SistemaCadastro.java
+++ DIO/Santander Bootcamp Java/aulas/exemplo-pacotes/src/main/java/contrutores/SistemaCadastro.java
@@ -1,6 +1,8 @@
+package contrutores;
+
 public class SistemaCadastro {
     public static void main(String[] args) {
-        Pessoa marcos = new Pessoa();
+        Pessoa marcos = new Pessoa("Paulo", "12345678910", "Reginaldo Nilson da Silva");
 
         marcos.setEndereco("RUA DAS MARIAS");
 
