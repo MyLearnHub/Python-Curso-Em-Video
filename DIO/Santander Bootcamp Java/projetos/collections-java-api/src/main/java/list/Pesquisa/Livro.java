2f65936aeb906c1064900367eddbf5eaa2ceb106
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-03 10:19:02 -0300

    Adicionando exercício 3 de Catálogo de Livros com Java usando List da DIO

--- DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/list/Pesquisa/Livro.java
+++ DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/list/Pesquisa/Livro.java
@@ -1,39 +1,38 @@
 package main.java.list.Pesquisa;
 
 public class Livro {
-  //atributos
-  private String titulo;
-  private String autor;
-  private int anoPublicacao;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
 
-  public Livro(String titulo, String autor, int anoPublicacao) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.anoPublicacao = anoPublicacao;
-  }
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
 
-  public String getTitulo() {
-    return titulo;
-  }
+    public String getTitulo() {
+        return titulo;
+    }
 
-  public void setAutor(String autor) {
-    this.autor = autor;
-  }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 
-  public String getAutor() {
-    return autor;
-  }
+    public String getAutor() {
+        return autor;
+    }
 
-  public int getAnoPublicacao() {
-    return anoPublicacao;
-  }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
 
-  @Override
-  public String toString() {
-    return "Livro{" +
-        "titulo='" + titulo + '\'' +
-        ", autor='" + autor + '\'' +
-        ", anoPublicacao=" + anoPublicacao +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
 }
