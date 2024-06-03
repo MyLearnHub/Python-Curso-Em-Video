f401e21a48b6629598a530711a9be47fb9281e1a
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-03 10:31:35 -0300

    Adicionando exercício s e  Comparable e Ordenação com va usando List da DIO

--- DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -5,54 +5,48 @@ import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoas {
-  //atributo
-  private List<Pessoa> pessoaList;
+    private List<Pessoa> pessoaList;
 
-  public OrdenacaoPessoas() {
-    this.pessoaList = new ArrayList<>();
-  }
-
-  public void adicionarPessoa(String nome, int idade, double altura) {
-    pessoaList.add(new Pessoa(nome, idade, altura));
-  }
-
-  public List<Pessoa> ordenarPorIdade() {
-    List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
-    if (!pessoaList.isEmpty()) {
-      Collections.sort(pessoasPorIdade);
-      return pessoasPorIdade;
-    } else {
-      throw new RuntimeException("A lista está vazia!");
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
     }
-  }
 
-  public List<Pessoa> ordenarPorAltura() {
-    List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
-    if (!pessoaList.isEmpty()) {
-      Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
-      return pessoasPorAltura;
-    } else {
-      throw new RuntimeException("A lista está vazia!");
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
     }
-  }
 
-  public static void main(String[] args) {
-  // Criando uma instância da classe OrdenacaoPessoas
-    OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        if (!pessoaList.isEmpty()) {
+            Collections.sort(pessoasPorIdade);
+            return pessoasPorIdade;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
 
-    // Adicionando pessoas à lista
-    ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
-    ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
-    ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
-    ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        if (!pessoaList.isEmpty()) {
+            Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+            return pessoasPorAltura;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
 
-    // Exibindo a lista de pessoas adicionadas
-    System.out.println(ordenacaoPessoas.pessoaList);
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
 
-    // Ordenando e exibindo por idade
-    System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
 
-    // Ordenando e exibindo por altura
-    System.out.println(ordenacaoPessoas.ordenarPorAltura());
-  }
+        System.out.println(ordenacaoPessoas.pessoaList);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
 }
