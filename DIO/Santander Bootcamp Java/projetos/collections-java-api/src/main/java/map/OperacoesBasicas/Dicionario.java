11d41a64712dc3b2cab97fddb357e8c56bc94199
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-04 05:19:13 -0300

    Adicionando conteúdo de uso de map em Java da DIO

--- DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/map/OperacoesBasicas/Dicionario.java
+++ DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/map/OperacoesBasicas/Dicionario.java
@@ -4,60 +4,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Dicionario {
-  private Map<String, String> dicionario;
+    private Map<String, String> dicionario;
 
-  public Dicionario() {
-    this.dicionario = new HashMap<>();
-  }
-
-  public void adicionarPalavra(String palavra, String definicao) {
-    dicionario.put(palavra, definicao);
-  }
-
-  public void removerPalavra(String palavra) {
-    if (!dicionario.isEmpty()) {
-      dicionario.remove(palavra);
-    } else {
-      System.out.println("O dicionário está vazio.");
+    public Dicionario() {
+        this.dicionario = new HashMap<>();
     }
-  }
 
-  public String pesquisarPorPalavra(String palavra) {
-    String definicao = dicionario.get(palavra);
-    if (definicao != null) {
-      return definicao;
+    public void adicionarPalavra(String palavra, String definicao) {
+        dicionario.put(palavra, definicao);
     }
-    return "Linguagem não encontrada no dicionário.";
-  }
 
-  public void exibirPalavras() {
-    if (!dicionario.isEmpty()) {
-      System.out.println(dicionario);
-    } else {
-      System.out.println("O dicionário está vazio.");
+    public void removerPalavra(String palavra) {
+        if (!dicionario.isEmpty()) {
+            dicionario.remove(palavra);
+        } else {
+            System.out.println("O dicionário está vazio.");
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    Dicionario dicionario = new Dicionario();
+    public String pesquisarPorPalavra(String palavra) {
+        String definicao = dicionario.get(palavra);
+        if (definicao != null) {
+            return definicao;
+        }
+        return "Linguagem não encontrada no dicionário.";
+    }
 
-    // Adicionar palavras (linguagens de programação)
-    dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
-    dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
-    dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
+    public void exibirPalavras() {
+        if (!dicionario.isEmpty()) {
+            System.out.println(dicionario);
+        } else {
+            System.out.println("O dicionário está vazio.");
+        }
+    }
 
-    // Exibir todas as palavras
-    dicionario.exibirPalavras();
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
 
-    // Pesquisar palavras
-    String definicaoJava = dicionario.pesquisarPorPalavra("java");
-    System.out.println("Definição da linguagem 'java': " + definicaoJava);
+        dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
+        dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
+        dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
 
-    String definicaoCSharp = dicionario.pesquisarPorPalavra("csharp");
-    System.out.println(definicaoCSharp);
+        dicionario.exibirPalavras();
 
-    // Remover uma palavra
-    dicionario.removerPalavra("typescript");
-    dicionario.exibirPalavras();
-  }
+        String definicaoJava = dicionario.pesquisarPorPalavra("java");
+        System.out.println("Definição da linguagem 'java': " + definicaoJava);
+
+        String definicaoCSharp = dicionario.pesquisarPorPalavra("csharp");
+        System.out.println(definicaoCSharp);
+
+        dicionario.removerPalavra("typescript");
+        dicionario.exibirPalavras();
+    }
 }
--- DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/map/Ordenacao/AgendaEventos.java
+++ DIO/Santander Bootcamp Java/projetos/collections-java-api/src/main/java/map/Ordenacao/AgendaEventos.java
@@ -5,58 +5,55 @@ import java.time.Month;
 import java.util.*;
 
 public class AgendaEventos {
-  private Map<LocalDate, Evento> eventosMap;
+    private Map<LocalDate, Evento> eventosMap;
 
-  public AgendaEventos() {
-    this.eventosMap = new HashMap<>();
-  }
-
-  public void adicionarEvento(LocalDate data, String nomeEvento, String descricaoAtracao) {
-    eventosMap.put(data, new Evento(nomeEvento, descricaoAtracao));
-  }
-
-  public void exibirAgenda() {
-    Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
-    for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
-      LocalDate dataEvento = entry.getKey();
-      Evento evento = entry.getValue();
-      System.out.println("Data: " + dataEvento + ", Evento: " + evento.getNome() + ", Atração: " + evento.getAtracao());
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
     }
-  }
 
-  public void obterProximoEvento() {
-    LocalDate dataAtual = LocalDate.now();
-    LocalDate proximaData = null;
-    Evento proximoEvento = null;
-    for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
-      LocalDate dataEvento = entry.getKey();
-      if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
-        proximaData = dataEvento;
-        proximoEvento = entry.getValue();
-        break;
-      }
+    public void adicionarEvento(LocalDate data, String nomeEvento, String descricaoAtracao) {
+        eventosMap.put(data, new Evento(nomeEvento, descricaoAtracao));
     }
-    if (proximoEvento != null) {
-      System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
-    } else {
-      System.out.println("Não há eventos futuros na agenda.");
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            Evento evento = entry.getValue();
+            System.out.println("Data: " + dataEvento + ", Evento: " + evento.getNome() + ", Atração: " + evento.getAtracao());
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    AgendaEventos agendaEventos = new AgendaEventos();
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximaData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+        if (proximoEvento != null) {
+            System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
+        } else {
+            System.out.println("Não há eventos futuros na agenda.");
+        }
+    }
 
-    // Adiciona eventos à agenda
-    agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
-    agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
-    agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
-    agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
-    agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
 
-    // Exibe a agenda completa de eventos
-    agendaEventos.exibirAgenda();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
 
-    // Obtém e exibe o próximo evento na agenda
-    agendaEventos.obterProximoEvento();
-  }
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
 }
