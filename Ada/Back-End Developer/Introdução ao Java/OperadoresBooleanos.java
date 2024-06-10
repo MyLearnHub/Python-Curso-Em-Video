3353f6d2ba3e8dfe6e2da41029da8ea1ae0a2688
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-10 03:08:27 -0300

    Adicionando conteúdo sobre operadores booleanos e ternários em Java da Ada

---
+++ Ada/Back-End Developer/Introdução ao Java/OperadoresBooleanos.java
@@ -0,0 +1,29 @@
+package bubbles.springapibackend;
+
+public class OperadoresBooleanos {
+    public static void main(String[] args) {
+        /*
+        boolean resultado = false;
+        System.out.println(resultado);
+        */
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        System.out.println(vamosAPraia);
+
+        String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        System.out.println(mensagem);
+        // Tabela verdade
+        // Operador && (AND)
+        // true && true = true
+        // true && false = false
+        // false && true = false
+        // false && false = false
+
+        // Operador || (OR)
+        // true || true = true
+        // true || false = true
+        // false || true = true
+        // false || false = false
+    }
+}
