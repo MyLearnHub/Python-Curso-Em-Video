5f7ec62aeb38b689953c8a53d6f2edc6355a9025
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-10 03:35:20 -0300

    Adicionando conteúdo de estruturas condicionais em Java da Ada

---
+++ Ada/Back-End Developer/Introdução ao Java/EstruturasCondicionais.java
@@ -0,0 +1,41 @@
+package bubbles.springapibackend;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+        int nota = 70;
+        String graduacao;
+
+        /*
+        if (nota >= 70) {
+            System.out.println("Aluno aprovado!");
+        } else {
+            System.out.println("Não aprovado.");
+        }
+        */
+
+        if (nota >= 80) {
+            graduacao = "Graduação A";
+        } else if (nota < 80 && nota >= 70) {
+            graduacao = "Graduação B";
+        } else if (nota < 70 && nota >= 60) {
+            graduacao = "Graduação C";
+        } else if (nota < 60 && nota >= 0) {
+            graduacao = "Graduação D";
+        } else {
+            graduacao = "";
+        }
+
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Não aprovado");
+                break;
+            default:
+                System.out.println("Graduação inválida");
+        }
+    }
+}
