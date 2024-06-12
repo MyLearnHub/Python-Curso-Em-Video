7ac15f256c0c91e3453cc7df28c7841197976f54
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-12 06:32:18 -0300

    Adicionando conteúdo sobre construtores e encapsulamento em Java da Ada

--- Ada/Back-End Developer/java-introduction/src/main/java/programacaoOrientadaObjeto/Animais/Cachorro.java
+++ Ada/Back-End Developer/java-introduction/src/main/java/programacaoOrientadaObjeto/Animais/Cachorro.java
@@ -1,12 +1,71 @@
 package programacaoOrientadaObjeto.Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
-    public String estadoDeEspirito;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
+
+    public Cachorro() {}
+
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
 
     public void comer() {
     }
@@ -20,6 +79,7 @@ public class Cachorro {
     }
 
     public String interagir(String acao) {
+        /*
         if (acao.equals("carinho")) {
             this.estadoDeEspirito = "feliz";
         } else if (acao.equals("vai dormir!")) {
@@ -28,5 +88,22 @@ public class Cachorro {
             this.estadoDeEspirito = "neutro";
         }
         return estadoDeEspirito;
+        */
+
+        switch (acao) {
+            case "carinho":
+                this.estadoDeEspirito = "feliz";
+                break;
+            case "vai dormir!":
+                this.estadoDeEspirito = "bravo";
+                break;
+            case "pisar na patinha":
+                this.estadoDeEspirito = "triste";
+                break;
+            default:
+                this.estadoDeEspirito = "neutro";
+        }
+
+        return this.estadoDeEspirito;
     }
 }
--- Ada/Back-End Developer/java-introduction/src/main/java/programacaoOrientadaObjeto/Animais/Main.java
+++ Ada/Back-End Developer/java-introduction/src/main/java/programacaoOrientadaObjeto/Animais/Main.java
@@ -3,14 +3,12 @@ package programacaoOrientadaObjeto.Animais;
 public class Main {
     public static void main(String[] args) {
         Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro("Puppy", "Marron", 25, 5.5, 5, "nada");
 
-        cachorro1.nome = "Puppy";
-        cachorro1.cor = "Marron";
-        cachorro1.altura = 25;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoDoRabo = 5;
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getPeso());
 
-        cachorro1.latir();
+
         System.out.println("O cachorro pegou uma " + cachorro1.pegar());
         System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
         System.out.println("O cachorro está " + cachorro1.interagir("nada"));
