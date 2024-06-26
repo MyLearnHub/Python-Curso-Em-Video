dd981a5a5b02516eebd435717d95ed2fd4d1b142
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-26 01:25:23 -0300

    Adicionando conteúdo de laço de repetição for em Python do Curso em Vídeo

---
+++ Curso em Vídeo/Python/aula13/for.py
@@ -0,0 +1,15 @@
+for c in range(1, 7):
+    print(c)
+print('FIM')
+
+print('-' * 11)
+
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+
+print('-' * 11)
+
+for c in range(1, 7, 2):
+    print(c)
+print('FIM')
